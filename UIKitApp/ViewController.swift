1d0ee8cb1b560c7229f55e83331224609cd60803
Egor Ukolov <ukolovegor@yandex.ru>
2022-10-31 22:09:02 +0700

    Настройка элементов экрана через код: Label, SegmentedControl. Добавлен Slider

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Label
+        mainLabel.text = ""
+        mainLabel.font = mainLabel.font.withSize(35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        
+        
+        // Segmented control
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+        
     }
 
-
+    @IBAction func chooseSegment() {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "The first segment is selected"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "The second segment is selected"
+            mainLabel.textColor = .yellow
+        case 2:
+            mainLabel.text = "The third segment is selected"
+            mainLabel.textColor = .blue
+        default: break
+        }
+    }
+    
 }
 
